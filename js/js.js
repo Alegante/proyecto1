2915ec82780e62b91fd67ad0412dbf02d13ccb36
Alegante <alejandro17.alazne@gmail.com>
2024-02-27 11:13:28 +0100

    nuevas cambios

---
+++ js/js.js
@@ -0,0 +1,21 @@
+    
+let user = new Object(); // "object constructor" syntax
+user = {};  // "object literal" syntax
+
+   user = {     // an object
+    name: "John",  // by key "name" store value "John"
+    age: 30        // by key "age" store value 30
+  };
+
+  // get property values of the object:
+alert( user.name ); // John
+alert( user.age ); // 30
+
+user = {
+  name: "ales",
+  age: 20
+};  
+
+
+
+class user 
